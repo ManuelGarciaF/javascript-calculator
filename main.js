61e4a14ece25d4a4f4546f089d880d52046f0711
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-10-25 18:11:57 -0300

    used regex to divide input

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// make an operation depending on the operator
 const operate = (operator, a, b) => {
     switch (operator) {
         case "+":
@@ -13,50 +14,74 @@ const operate = (operator, a, b) => {
     }
 };
 
+// Updates the display in the html.
 const updateDisplay = () => {
-    document.querySelector(".display").innerHTML = displayText.join("");
+    document.querySelector(".display").innerHTML = displayText;
+};
+
+const showError = () => {
+    // TODO: Finish this.
 };
 
 const getResult = () => {
-    let parsed = [];
-    let currentNumber = "";
-
-    displayText.forEach(elem => {
-        if (elem === "+" || elem === "-" || elem === "÷" || elem === "×") {
-            if (currentNumber) {
-                parsed.push(Number(currentNumber));
-                currentNumber = "";
+    const calculate = arr => {
+        console.log(arr.length);
+        for (let i = 0, len = arr.length; i < len; i++) {
+            if (
+                arr[i] !== "+" &&
+                arr[i] !== "-" &&
+                arr[i] !== "÷" &&
+                arr[i] !== "×"
+            ) {
+                console.log("continued...")
+                continue;
             }
 
-            parsed.push(elem);
-        } else {
-            currentNumber = currentNumber.concat(elem);
+            let numA = arr[i - 1];
+            let operator = arr[i];
+            let numB = arr[i + 1];
+
+            arr[i + 1] = operate(operator, numA, numB);
+            arr.shift();
+            arr.shift();
+            console.log(arr);
+            console.log({ i })
         }
-    });
+        return arr;
+    };
+
+    const operations = displayText
+        .split(/(\+|-|÷|×)/)
+        .map(elem => (isNaN(elem) ? elem : Number(elem)));
+
+    console.log(operations);
+
+    calculate(operations);
 };
 
-let displayText = [];
+// Holds what is displayed.
+let displayText = "";
 
 document.querySelectorAll(".keypad button").forEach(elem => {
     if (displayText.length < 10) {
         switch (elem.dataset.key) {
             case "=":
                 elem.addEventListener("click", () => {
-                    console.log({ displayText });
                     getResult();
+                    console.log({ displayText });
                 });
                 break;
 
             case "C":
                 elem.addEventListener("click", () => {
-                    displayText = [];
+                    displayText = "";
                     updateDisplay();
                 });
                 break;
 
             default:
                 elem.addEventListener("click", event => {
-                    displayText.push(event.target.dataset.key);
+                    displayText = displayText.concat(event.target.dataset.key);
                     updateDisplay();
                 });
                 break;
